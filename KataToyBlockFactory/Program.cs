87238a877a41abf70defa1dd537f0eaacd563b51
Michelle <wong.mich.c@gmail.com>
2020-07-16 08:46:05 +1000

    first commit

---
+++ KataToyBlockFactory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KataToyBlockFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
