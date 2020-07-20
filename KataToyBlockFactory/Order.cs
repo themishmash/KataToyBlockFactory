fc02a5e119a2a9cb1cfab481bd05aa058dca3050
Michelle <wong.mich.c@gmail.com>
2020-07-20 11:18:41 +1000

    created test double orderinput class. created order class and block class. Need to create interface

--- KataToyBlockFactory/Order.cs
+++ KataToyBlockFactory/Order.cs
@@ -1,34 +1,48 @@
+using System;
 using System.Collections.Generic;
 namespace KataToyBlockFactory
 {
     public class Order
     {
+        //private readonly int _orderNumber;
+
         public string Name { get; }
         public OrderStatus OrderStatus { get; private set; }
         public string Address { get; private set; }
-        public string Date { get; private set; }
+        //public string Date { get; private set; }
+        
+        public DateTime Date { get; }
         public int OrderNumber { get; private set; }
         
-        private readonly List<Block> _blocks = new List<Block>(); 
+        public readonly List<Block> Blocks = new List<Block>();
+        
+        // public Order(string name, string address, string date, int orderNumber, OrderStatus orderStatus = 
+        // OrderStatus.New)
+        // {
+        //     Name = name;
+        //     Address = address;
+        //     Date = date;
+        //     OrderNumber = orderNumber;
+        //     OrderStatus = orderStatus;
+        // }
 
-        public Order(string name, string address, string date, int orderNumber, OrderStatus orderStatus = 
-        OrderStatus.New)
+
+        public Order(string name, string address, DateTime date, int orderNumber, List<Block> blocks)
         {
+            
             Name = name;
             Address = address;
             Date = date;
             OrderNumber = orderNumber;
-            OrderStatus = orderStatus;
+            Blocks = blocks;
         }
-        
 
-
-        public int GetBlockQuantity()
-        {
-            var block = new Block(); //this will eventually match the input of user
-            _blocks.Add(block);
-            return _blocks.Count;
-        }
+        // public int GetBlockQuantity()
+        // {
+        //     var block = new Block(); //this will eventually match the input of user
+        //     _blocks.Add(block);
+        //     return _blocks.Count;
+        // }
     }
     
 }
--- ToyBlockFactory.Tests/OrderTest.cs
+++ ToyBlockFactory.Tests/OrderTest.cs
@@ -22,14 +22,29 @@ namespace ToyBlockFactory.Tests
         [Fact]
         public void OrdersHaveAllNecessaryFields()
         {
-            var order = new Order("James", "123 Smith Street, Fitzroy", "20 October 2020", 1);
-
+           //  var order = new Order("James", "123 Smith Street, Fitzroy", "20 October 2020", 1);
+           //
+           //  Assert.Equal(OrderStatus.New, order.OrderStatus);
+           //  Assert.Equal("James", order.Name);
+           //  Assert.Equal("123 Smith Street, Fitzroy", order.Address);
+           // // Assert.Equal("20 October 2020", order.Date);
+           //  Assert.Equal(1, order.OrderNumber);
+           //  Assert.Equal(1, order.GetBlockQuantity());
+        }
+        
+        [Fact]
+        public void OrdersHaveInput()
+        {
+            var orderInput = new OrderInput("James", "123 Smith Street, Fitzroy", "10/10/2020", "5");
+            var order = new Order(orderInput.Name, orderInput.Address, orderInput.Date, orderInput.OrderNumber, orderInput.BlockOrder);
+            var expectedDate = orderInput.Date;
+            
             Assert.Equal(OrderStatus.New, order.OrderStatus);
             Assert.Equal("James", order.Name);
             Assert.Equal("123 Smith Street, Fitzroy", order.Address);
-            Assert.Equal("20 October 2020", order.Date);
+            Assert.Equal(expectedDate, order.Date);
             Assert.Equal(1, order.OrderNumber);
-            Assert.Equal(1, order.GetBlockQuantity());
+            Assert.Equal(5, order.Blocks.Count);
         }
     }
 }
