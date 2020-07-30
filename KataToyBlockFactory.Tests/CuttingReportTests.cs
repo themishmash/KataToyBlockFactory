2ffa9c91ebe5e5d8c4bcec45a3317efefcdbf87c
Michelle <wong.mich.c@gmail.com>
2020-07-30 17:15:25 +1000

    refactored cutting report tests so have setup

--- KataToyBlockFactory.Tests/CuttingReportTests.cs
+++ KataToyBlockFactory.Tests/CuttingReportTests.cs
@@ -19,7 +19,7 @@ namespace KataToyBlockFactory.Tests
         }
         
         [Fact]
-        public void Return_Total_Based_On_Order_Number()
+        public void Total_Number_Of_Shapes_Is_Based_On_Order_Number()
         {
             //Act
             var cuttingReportForOrder1 = _toyBlockFactory.GetCuttingReport(1);
@@ -28,59 +28,30 @@ namespace KataToyBlockFactory.Tests
             Assert.Equal(2, cuttingReportForOrder1.GetShapeCount(Shape.Circle));
         }
 
-        
-
-        [Fact]
-        public void Shapes_Not_Cut_In_Daily_Total()
-        {
-            //arrange
-            var toyBlockFactory = new ToyBlockFactory();
-            var order = toyBlockFactory.CreateOrder("James", "123 Smith Street, Fitzroy");
-            order.AddBlock(Shape.Circle, Color.Blue);
-            order.AddBlock(Shape.Circle, Color.Red);
-
-            var order2 = toyBlockFactory.CreateOrder("", "");
-            order2.AddBlock(Shape.Circle, Color.Blue);
-
-            //Act
-            var cuttingReport = toyBlockFactory.GetDailyCuttingReport();
-            
-            //assert
-            Assert.Equal(0, cuttingReport.GetShapeCount(Shape.Triangle));
-        }
-
         [Fact]
         public void Daily_Total()
         {
-            var toyBlockFactory = new ToyBlockFactory();
-            var order = toyBlockFactory.CreateOrder("James", "123 Smith Street, Fitzroy");
-            order.AddBlock(Shape.Circle, Color.Blue);
-            order.AddBlock(Shape.Circle, Color.Red);
 
-            var order2 = toyBlockFactory.CreateOrder("", "");
-            order2.AddBlock(Shape.Circle, Color.Blue);
-
-            var cuttingReport = toyBlockFactory.GetDailyCuttingReport();
-            Assert.Equal(3, cuttingReport.GetShapeCount(Shape.Circle));
+            var cuttingReportForDailyTotal = _toyBlockFactory.GetDailyCuttingReport();
+            Assert.Equal(3, cuttingReportForDailyTotal.GetShapeCount(Shape.Circle));
         }
 
         [Fact]
         public void Default_Count_Set_To_Zero()
         {
-            var toyBlockFactory = new ToyBlockFactory();
-            var order = toyBlockFactory.CreateOrder("James", "123 Smith Street, Fitzroy");
-            order.AddBlock(Shape.Circle, Color.Blue);
-            order.AddBlock(Shape.Circle, Color.Red);
-
-            var order2 = toyBlockFactory.CreateOrder("", "");
-            order2.AddBlock(Shape.Circle, Color.Blue);
-
-            var cuttingReport = toyBlockFactory.GetCuttingReport(1);
+            var cuttingReport = _toyBlockFactory.GetCuttingReport(1);
             
             Assert.Equal(0, cuttingReport.GetShapeCount(Shape.Triangle));
         }
         
-        //todo repeat above test for daily cutting report. 
-        
+        [Fact]
+        public void Shapes_Not_Cut_In_Daily_Total()
+        {
+            var cuttingReport = _toyBlockFactory.GetDailyCuttingReport();
+            
+            //assert
+            Assert.Equal(0, cuttingReport.GetShapeCount(Shape.Triangle));
+        }
+
     }
 }
